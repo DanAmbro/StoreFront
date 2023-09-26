4b582600bf2069b47c7596309ca8e1906456dd56
DanielAmbro <DanielAmbro1@outlook.com>
2023-09-25 20:15:25 -0500

    Adding more parts

--- StoreFront.UI.MVC/Program.cs
+++ StoreFront.UI.MVC/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using StoreFront.UI.MVC.Data;
+using System.Security.Principal;
 
 namespace StoreFront.UI.MVC
 {
@@ -16,8 +17,8 @@ namespace StoreFront.UI.MVC
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddRoles < IdentityRole > ().AddRoleManager < RoleManager < IdentityRole >>
+                 ().AddEntityFrameworkStores<ApplicationDbContext>();
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
